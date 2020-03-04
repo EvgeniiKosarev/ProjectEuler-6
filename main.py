5b9ccc8314575f8e112e94f01e305c44991bbf19
EvgeniiKosarev <evgenii.kosarev@yahoo.co.uk>
2020-03-04 16:48:51 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import numpy
 
-def smm(n):
-    '''
-    (int)-> int
 
-    Function returns the smallest natural number, that can be divided by numbers
-    1 to n, inclusive, with no remainder.
+def diff(n):
+
+    '''
+    (int) -> int
+
+    Function returns the difference between the square of the sum and
+    the sum of squares of the first n natural numbers
+
+    >>> diff(10):
+    2640
+
 
     '''
 
     
-    multiples = [1]
-    for i in range(2, n+1):
-        remainder = 0
-        for k in multiples:
-            if i%k ==0:
-                i = i//k
-            remainder = i
-        else:
-            if remainder != 0:
-                multiples.append(remainder)
+    sq_sm = 0
+    sm_sq = 0
+    for i in range(n+1):
+        sq_sm+=i
+        sm_sq += i**2
 
-    return numpy.prod(multiples)
+    return sq_sm**2 - sm_sq
+
+    
+    
 
 
